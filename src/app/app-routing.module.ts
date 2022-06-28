94285208c07f23557738c922f09e004b3330685d
Luis Morales <dev.luismorales@gmail.com>
2022-06-28 11:34:41 -0500

    se hizo el lazyload y el sistemas de rutas de la aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then( module => module.AuthModule) },
+  { path: 'dashboard', loadChildren: () => import('./protected/protected.module').then( module => module.ProtectedModule) },
+  { path: '**', redirectTo: 'auth' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
